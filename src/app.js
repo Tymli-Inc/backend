d44b7c0bd4adc0d8c4474c7e5688a82da0accef4
AnmolPlayzz <anmolisnotavailable@gmail.com>
2025-06-24 13:46:31 +0530

    Fix static file serving path in Express configuration

--- src/app.js
+++ src/app.js
@@ -20,7 +20,7 @@ app.use(cors({
 
 app.use(express.json({limit: '50mb'}));
 app.use(express.urlencoded({extended: true, limit: '50mb'}));
-app.use(express.static('public'));
+app.use(express.static(path.join(__dirname, '../public')));
 
 app.use(session({
   secret: process.env.SESSION_SECRET,
